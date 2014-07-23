7a1d6432364de4cca80afc4aec508175a8a0930f
Kevin Albert <kevin.albert@productops.com>
2014-07-22 21:23:23 -0700

    Started on thread stuff

--- headers/quadcopter.h
+++ headers/quadcopter.h
@@ -1,12 +1,52 @@
 #pragma once
 
+#include <mutex>
+#include <thread>
 #include "motor.h"
 
 class QuadCopter {
     public:
+        
+        /**
+         * initialize everything
+         * start the loop
+         */
         static void start();
-        static void do(char c);
+
+        /**
+         * Process an instruction
+         */
+        static void do(char);
+
+        /**
+         * Set the interrupt flag
+         * join the loop thread
+         * turn off gyroscope
+         * shutdown
+         */
         static void stop();
+
+        /**
+         * Perform an iteration of the loop
+         */
+        static bool tick();
+
     private:
-        Motor** motors;
+
+        // Threads
+        static std::thread* loopThread;
+        static std::mutex* lock;
+        static bool interrupt;
+
+        // Motor layout:
+        // 0   1
+        //  \ /
+        //   X
+        //  / \
+        // 2   3
+        static Motor* motors[4];
+        static const int TOP_LEFT       = 0;
+        static const int TOP_RIGHT      = 1;
+        static const int BOTTOM_LEFT    = 2;
+        static const int BOTTOM_RIGHT   = 3;
 }
--- src/quadcopter.cpp
+++ src/quadcopter.cpp
@@ -4,18 +4,36 @@
 #include "motor.h"
 #include "quadcopter.h"
 
+
 public static void QuadCopter::start() {
-    motors = new (Motor())[4];
+
+
+    interrupt = false;
+
+    motors[0] = new Motor(15);
+    motors[1] = new Motor(18);
+    motors[2] = new Motor(23);
+    motors[3] = new Motor(24);
+
     std::cout << "start\n";
     Gyro::start();
     for (int i = 0; i < 100; i++) {
         usleep(100000);
         Gyro::read();
     }
+
+    // Start the loop!
+    loopThread = new std::thread(QuadCopter::tick);
 }
 
 public static void QuadCopter::do(char c) {
     Gyro::stop();
     std::cout << "stop";
+}
+
+public static void QuadCopter::stop() {
+    delete[] motors;
+    interrupt = true;
+    loopThread->join();
 }
 
